bbe87ca5e5ab5974d4a867352ed107675f4d8722
Colin Steele <cvillecsteele@gmail.com>
2012-10-01 11:36:40 -0400

    For deploy to clojars.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-git-version "1.0.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject org.clojars.cvillecsteele/lein-git-version "1.0.0"
+  :description "Use git for project versions"
+  :url "https://github.com/cvillecsteele/lein-git-version"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
