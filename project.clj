2d2cfc1da3af065abc95599781283321adce17df
Jason Ozias <jason.g.ozias@gmail.com>
2013-12-12 11:51:40 -0500

    plugin work

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
-  :profiles
-  {:dev {:dependencies [[midje "1.4.0"]]
-         :plugins [[lein-midje "2.0.0-SNAPSHOT"]]}})
+  :profiles {:dev {:dependencies [[midje "1.4.0"]
+                                  [org.clojure/clojure "1.5.1"]]
+                   :plugins [[lein-midje "2.0.0-SNAPSHOT"]]}})
--- src/leiningen/git_version.clj
+++ src/leiningen/git_version.clj
@@ -1,23 +1,16 @@
 (ns leiningen.git-version
-  (:require [leiningen.help]
-            [leiningen.jar]
-            [leiningen.compile]
-            [leiningen.core.main]
-            [leiningen.core.project]
-            [robert.hooke]
-            [leiningen.test])
-  (:use
-   [clojure.java.shell :only [sh]]))
+  (:require [clojure.java.shell :refer [sh]]
+            [clojure.string :as str :refer [trim]]))
 
-(defn get-git-version
-  []
-  (apply str (rest (clojure.string/trim
-                    (:out (sh
-                           "git" "describe" "--match" "v*.*"
-                           "--abbrev=4" "--dirty=**DIRTY**"))))))
+(def ^:private defaults
+  {:version-command ["git" "describe" "--match" "v*.*" "--abbrev=4" "--dirty=**DIRTY**"]
+   :out->version '(fn [tag] tag)})
+
+(defn get-git-version [& [config]]
+  (let [{:keys [version-command out->version]} (merge defaults config)]
+    ((eval out->version) (str/trim (:out (apply sh version-command))))))
 
 (defn git-version
-  "Main project task."
-  ^{:doc "Show git project version"}
+  "Show git project version"
   [project & args]
-  (println (get-git-version)))
+  (println (get-git-version (:git-version project))))
