abf2bf20fd2d6a02b07a36da3060f3c52e7d091e
Jason Ozias <jason.g.ozias@gmail.com>
2014-01-18 16:31:29 -0500

    Added support for :version-file-command command to allow version file to differ from :version in project.clj

--- src/leiningen/git_version.clj
+++ src/leiningen/git_version.clj
@@ -4,13 +4,18 @@
 
 (def ^:private defaults
   {:version-command ["git" "describe" "--match" "v*.*" "--abbrev=4" "--dirty=**DIRTY**"]
+   :version-file-command ["git" "describe" "--match" "v*.*" "--abbrev=4" "--dirty=**DIRTY**"]
    :out->version '(fn [tag] tag)})
 
-(defn get-git-version [& [config]]
+(defn get-version [& [config]]
   (let [{:keys [version-command out->version]} (merge defaults config)]
     ((eval out->version) (str/trim (:out (apply sh version-command))))))
+
+(defn get-file-version [& [config]]
+  (let [{:keys [version-file-command out->version]} (merge defaults config)]
+    ((eval out->version) (str/trim (:out (apply sh version-file-command))))))
 
 (defn git-version
   "Show git project version"
   [project & args]
-  (println (get-git-version (:git-version project))))
+  (println (get-version (:git-version project))))
