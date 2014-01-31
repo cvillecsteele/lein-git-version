72842e49d091e485d256c894e7fa015a770cfc6a
Jason Ozias <jason.g.ozias@gmail.com>
2014-01-30 19:45:56 -0500

    function cleanup

--- src/leiningen/git_version.clj
+++ src/leiningen/git_version.clj
@@ -7,17 +7,15 @@
   {:version-command ["git" "describe" "--match" "v*.*" "--abbrev=4" "--dirty=**DIRTY**"]
    :version-file-command ["git" "describe" "--match" "v*.*" "--abbrev=4" "--dirty=**DIRTY**"]})
 
-(defn get-version [project]
+(defn get-version [project & file]
   (let [config (:git-version project)
         version (:version project)
-        {:keys [version-command]} (merge defaults config)]
-    (str version "+" (str/trim (:out (apply sh version-command))))))
-
-(defn get-file-version [project]
-  (let [config (:git-version project)
-        version (apply str (take-while #(not (= \+ %)) (:version project)))
-        {:keys [version-file-command]} (merge defaults config)]
-    (str version "+" (str/trim (:out (apply sh version-file-command))))))
+        {:keys [version-command version-file-command]} (merge defaults config)
+        cmd (if (seq file) version-file-command version-command)
+        shortver (apply str (take-while #(not (= \+ %)) version))]
+    (if (seq file)
+      (str shortver "+" (str/trim (:out (apply sh cmd))))
+      (str version "+" (str/trim (:out (apply sh cmd)))))))
 
 (defn git-version
   "Show git project version"
