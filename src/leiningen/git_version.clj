54d79385676bf5f4ea2e997ed957c871429f2ff5
Jason Ozias <jason.g.ozias@gmail.com>
2014-01-30 19:09:14 -0500

    Some cleanup for semver stuff

--- src/leiningen/git_version.clj
+++ src/leiningen/git_version.clj
@@ -1,25 +1,25 @@
 (ns leiningen.git-version
   (:require [clojure.java.shell :refer [sh]]
+            [clojure.pprint :refer [pprint]]
             [clojure.string :as str :refer [trim]]))
 
 (def ^:private defaults
   {:version-command ["git" "describe" "--match" "v*.*" "--abbrev=4" "--dirty=**DIRTY**"]
-   :version-file-command ["git" "describe" "--match" "v*.*" "--abbrev=4" "--dirty=**DIRTY**"]
-   :out->version '(fn [version tag] (str version "+" tag))})
+   :version-file-command ["git" "describe" "--match" "v*.*" "--abbrev=4" "--dirty=**DIRTY**"]})
 
 (defn get-version [project]
   (let [config (:git-version project)
         version (:version project)
-        {:keys [version-command out->version]} (merge defaults config)]
-    ((eval out->version) version (str/trim (:out (apply sh version-command))))))
+        {:keys [version-command]} (merge defaults config)]
+    (str version "+" (str/trim (:out (apply sh version-command))))))
 
 (defn get-file-version [project]
   (let [config (:git-version project)
         version (:version project)
-        {:keys [version-file-command out->version]} (merge defaults config)]
-    ((eval out->version) version (str/trim (:out (apply sh version-file-command))))))
+        {:keys [version-file-command]} (merge defaults config)]
+    (str version "+" (str/trim (:out (apply sh version-file-command))))))
 
 (defn git-version
   "Show git project version"
   [project & args]
-  (println (get-version project)))
+  (println (:version project)))
