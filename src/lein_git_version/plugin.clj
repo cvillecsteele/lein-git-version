b0be870ba50f58caa8f02fe36a4805ea8fe3e171
Colin Steele <cvillecsteele@gmail.com>
2014-07-14 16:29:00 -0400

    Allow version path to be configured.

--- src/lein_git_version/plugin.clj
+++ src/lein_git_version/plugin.clj
@@ -10,8 +10,10 @@
               "(ns " (:name project) ".version)\n"
               "(def version \"" (get-git-version) "\")\n")
         proj-dir (.toLowerCase (.replace (:name project) \- \_))
-        filename (str (or (first (:source-paths project)) "src") "/"
-                      proj-dir "/version.clj")]
+        filename (if (:git-version-path project)
+                   (str (:git-version-path project) "/version.clj")
+                   (str (or (first (:source-paths project)) "src") "/"
+                        proj-dir "/version.clj"))]
     (-> project
         (update-in [:injections] concat `[(spit ~filename ~code)])
         (assoc :version (get-git-version)))))
