4319ae211e0008448c17f8a8ce3b1eda43251b77
Colin Steele <cvillecsteele@gmail.com>
2016-11-13 23:57:33 -0500

    Update readme.

--- project.clj
+++ project.clj
@@ -4,6 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
+
+  :signing {:gpg-key "E4DD670B"}
+  :deploy-repositories [["releases" :clojars]]
+
   :profiles
   {:dev {:dependencies [[midje "1.8.3"]]
          :plugins [[lein-midje "3.2"]]}})
